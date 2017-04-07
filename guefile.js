925e8fd1ddd0754601c6fc8d89324f017d30b933
Andrew Williams <andrew@manor.org>
2017-04-07 00:00:07 -0400

    Adding guefile

---
+++ guefile.js
@@ -0,0 +1,20 @@
+const gue = require('./index.js');
+
+gue.task('default', ['lint','test']);
+
+gue.task('test', () => {
+  return gue.shell('nyc mocha test/**/*.test.js');
+});
+
+gue.task('lint', () => {
+  return gue.shell('jscs index.js bin/gue.js lib/Util.js');
+});
+
+gue.task('docs', () => {
+  var command = '/bin/rm -f README.md';
+  command += '&& jsdoc2md --example-lang js --template docs/readme.hbs ';
+  command += '--partial docs/scope.hbs --separators --files index.js';
+  command += '> README.md';
+
+  return gue.shell(command);
+});
