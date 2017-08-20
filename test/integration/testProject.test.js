c74127dbaf6d5bc585f131445d010cc4d0f19e7e
Andrew Williams <andrew@manor.org>
2017-08-20 13:39:52 -0400

    Set path to gue.js for travis tests

--- test/integration/testProject.test.js
+++ test/integration/testProject.test.js
@@ -10,29 +10,29 @@ let runList = [];
 // Buld the list of integration tests to snapshot
 runList.push({
     name: 'simple fail test',
-    fn: 'gue --config test/integration/sampleTests/fail.guefile.js fail',
+    fn: './bin/gue.js --config test/integration/sampleTests/fail.guefile.js fail',
 });
 
 runList.push({
     name: 'simple succeed test',
-    fn: 'gue --config test/integration/sampleTests/succeed.guefile.js succeed',
+    fn: './bin/gue.js --config test/integration/sampleTests/succeed.guefile.js succeed',
 });
 
 runList.push({
     name: 'nested succeed test',
-    fn: 'gue --config test/integration/sampleTests/nestedSucceed.guefile.js'
+    fn: './bin/gue.js --config test/integration/sampleTests/nestedSucceed.guefile.js'
        + ' succeed',
 });
 
 runList.push({
     name: 'nested fail test',
-    fn: 'gue --config test/integration/sampleTests/nestedFail.guefile.js fail',
+    fn: './bin/gue.js --config test/integration/sampleTests/nestedFail.guefile.js fail',
 });
 
 // Check the snapshots for each of the guefiles above
 describe('Integration Tests', () => {
   it('should set the exit code to 0 on success', () => {
-    return execa.shell('gue --config '
+    return execa.shell('./bin/gue.js --config '
       + 'test/integration/sampleTests/succeed.guefile.js succeed')
     .then((result) => {
       return expect(result.code).to.equal(0);
@@ -40,7 +40,7 @@ describe('Integration Tests', () => {
   });
 
   it('should set the exit code to 1 on failure', () => {
-    return execa.shell('gue --config '
+    return execa.shell('./bin/gue.js --config '
       + 'test/integration/sampleTests/nestedFail.guefile.js fail')
     .catch((result) => {
       return expect(result.code).to.equal(1);
