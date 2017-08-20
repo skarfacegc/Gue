61186443d658750b98dcb9d40282de33c9e25693
Andrew Williams <andrew@manor.org>
2017-08-20 14:47:17 -0400

    Try to stabalize time a bit

--- test/integration/testProject.test.js
+++ test/integration/testProject.test.js
@@ -4,29 +4,35 @@ const chai = require('chai');
 const expect = chai.expect;
 const execa = require('execa');
 const snapshot = require('snap-shot');
+const sinon = require('sinon');
+
+const GueTask = require('../../lib/GueTask');
 
 let runList = [];
 
 // Buld the list of integration tests to snapshot
 runList.push({
-    name: 'simple fail test',
-    fn: './bin/gue.js --config test/integration/sampleTests/fail.guefile.js fail',
+  name: 'simple fail test',
+  fn: './bin/gue.js --config test/integration/sampleTests/fail.guefile.js fail',
 });
 
 runList.push({
-    name: 'simple succeed test',
-    fn: './bin/gue.js --config test/integration/sampleTests/succeed.guefile.js succeed',
+  name: 'simple succeed test',
+  fn: './bin/gue.js '
+    +'--config test/integration/sampleTests/succeed.guefile.js succeed',
 });
 
 runList.push({
-    name: 'nested succeed test',
-    fn: './bin/gue.js --config test/integration/sampleTests/nestedSucceed.guefile.js'
-       + ' succeed',
+  name: 'nested succeed test',
+  fn: './bin/gue.js '
+     +'--config test/integration/sampleTests/nestedSucceed.guefile.js'
+     + ' succeed',
 });
 
 runList.push({
-    name: 'nested fail test',
-    fn: './bin/gue.js --config test/integration/sampleTests/nestedFail.guefile.js fail',
+  name: 'nested fail test',
+  fn: './bin/gue.js '
+    + '--config test/integration/sampleTests/nestedFail.guefile.js fail',
 });
 
 // Check the snapshots for each of the guefiles above
@@ -50,13 +56,13 @@ describe('Integration Tests', () => {
   describe('Snapshot Tests', () => {
     runList.forEach((element) => {
       it(element.name + ' should match snapshot', () => {
-        return execa.shell('NODE_ENV=snapshot && ' + element.fn)
+        return execa.shell('export NODE_ENV=snapshot && '+element.fn)
         .then(
           (result) => {
-            return snapshot(result.stdout, false);
+            return snapshot(result.stdout);
           },
           (result) => {
-            return snapshot(result.stdout, false);
+            return snapshot(result.stdout);
           }
         );
       });
