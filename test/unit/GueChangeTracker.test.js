5c9fe4798b61b4c92ae23372470fd4003ebb43a9
Andrew Williams <andrew@manor.org>
2018-01-28 16:41:34 -0500

    Constructor argument handling in ChangeTracker
    related to issue #25

--- test/unit/GueChangeTracker.test.js
+++ test/unit/GueChangeTracker.test.js
@@ -6,8 +6,6 @@ const sinonChai = require('sinon-chai');
 const expect = chai.expect;
 const sandbox = sinon.sandbox.create();
 const ChaiAsPromised = require('chai-as-promised');
-const chalk = require('chalk');
-const chokidar = require('chokidar');
 
 const FileSet = require('../../lib/fileSet');
 const GueChangeTracker = require('../../lib/GueChangeTracker');
@@ -23,8 +21,17 @@ describe('GueChangeTracker', () => {
   describe('constructor', () => {
     it('should create an GueChangeTracker object', () => {
       const gueChangeTracker = new GueChangeTracker();
-
       expect(gueChangeTracker).to.be.instanceOf(GueChangeTracker);
+    });
+
+    it('should set fileSet correctly', () => {
+      const gueChangeTracker = new GueChangeTracker(new FileSet());
+      expect(gueChangeTracker.fileSet).to.be.instanceOf(FileSet);
+    });
+
+    it('should set dbFile correctly', () => {
+      const gueChangeTracker = new GueChangeTracker(new FileSet(), '.cache');
+      expect(gueChangeTracker.dbFile).to.equal('.cache');
     });
   });
 });
